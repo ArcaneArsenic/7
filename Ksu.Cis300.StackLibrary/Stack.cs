a338fcd94d084a5ebbbbd87722c8fa9088262700
ArcaneArsenic <tclevi@ksu.edu>
2020-06-16 18:52:48 -0500

    suo

---
+++ Ksu.Cis300.StackLibrary/Stack.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.StackLibrary
+{
+    public class Stack
+    {
+
+    }
+    /// <summary>
+    /// T def, add a type parameter T to the class statement,
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Stack<T>
+    {
+        private T[] ;
+    }
+}
